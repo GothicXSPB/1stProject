590738748971f27d6f71439600d6632b4ac56b24
andreymuskat <andrey.strel.2001@gmail.com>
2022-12-10 21:11:52 +0300

    Add class Database

---
+++ 1stProject/Database.cs
@@ -0,0 +1,12 @@
+﻿namespace _1stProject
+{
+    public class Database
+    {
+        public List<AdminClass> Admins { get; set; }
+
+        public List<Employee> Employees { get; set; }
+
+        public List<int> Calendar { get; set; }
+
+    }
+}
