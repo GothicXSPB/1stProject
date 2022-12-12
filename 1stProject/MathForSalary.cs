ffa3280f61eea8822b427cc144624679237cb264
Katerina Korotkaya <leanworld2020@gmail.com>
2022-12-12 19:02:08 +0300

    edit empty method

--- 1stProject/MathForSalary.cs
+++ 1stProject/MathForSalary.cs
@@ -2,5 +2,26 @@
 {
     static class MathForSalary
     {
+        const int MaxHoursInMonth = 164;
+
+        public static int CalculateWorkingHours()
+        {
+            return 1;
+        }
+
+        public static int CalculateOvertimeHours()
+        {
+            return 1;
+        }
+
+        public static int CheckingHolidayShifts()
+        {
+            return 1;
+        }
+
+        public static int CheckThatOvertimesLessThanMaxByTheLaw()
+        {
+            return 1;
+        }
     }
 }
