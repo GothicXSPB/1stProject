2d9222755854198b627818995de003b3386fbef7
andreymuskat <andrey.strel.2001@gmail.com>
2022-12-12 23:50:58 +0300

    Add method AddEmployee

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -1,1 +1,7 @@
 ﻿using _1stProject;
+
+Employee employee1 = new Employee(1, "Andrey1", "88005553535", "5/2");
+
+AdminClass admin = new AdminClass(2, "Andrey2", "89301654545", "3/2");
+
+admin.AddEmployee(employee1);
