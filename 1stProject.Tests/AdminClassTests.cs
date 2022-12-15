a4ef628d82cb12a6ad2f01a235f31301d6358fc8
andreymuskat <andrey.strel.2001@gmail.com>
2022-12-15 17:27:37 +0300

    Add Test AddEmployeeTest

--- 1stProject.Tests/AdminClassTests.cs
+++ 1stProject.Tests/AdminClassTests.cs
@@ -1,9 +1,28 @@
 using _1stProject;
+using _1stProject.Options;
+using NUnit.Framework;
 
 namespace _1stProject.Tests
 {
     public class AdminClassTests
     {
-        
+        [Test]
+        public void AddEmployeeTest()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
+            };
+
+            Employee employee1 = new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2);
+            
+            AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
+
+            admin.AddEmployee(employee1);
+
+            List<Employee> expectedEmployees = employees;
+            List<Employee> actualEmployees = Database.Employees;
+            CollectionAssert.AreEqual(expectedEmployees, actualEmployees);
+        }
     }
 }
