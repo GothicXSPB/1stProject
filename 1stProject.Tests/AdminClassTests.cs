fe397b87253bf227389b34bc9da3932d4faccb0b
andreymuskat <andrey.strel.2001@gmail.com>
2022-12-15 18:27:06 +0300

    Add test method DeleteEmployeeTest

--- 1stProject.Tests/AdminClassTests.cs
+++ 1stProject.Tests/AdminClassTests.cs
@@ -1,9 +1,33 @@
+using System.Xml;
 using _1stProject;
+using _1stProject.Options;
+using NUnit.Framework;
 
 namespace _1stProject.Tests
 {
     public class AdminClassTests
     {
-        
+        [Test]
+        public void DeleteEmployeeTest()
+        {
+            List<Employee> employees1 = new List<Employee>()
+            {
+                new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
+            };
+
+            Database.Employees = new List<Employee>()
+            {
+              new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
+              new Employee(2, "Stepan", "88005553535", TimeTable.Shift2x2),
+            };
+
+            AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
+
+            admin.DeleteEmployee(2);
+
+            List<Employee> expectedEmployees = employees1;
+            List<Employee> actualEmployees = Database.Employees;
+            CollectionAssert.AreEqual(expectedEmployees, actualEmployees);
+        }
     }
 }
