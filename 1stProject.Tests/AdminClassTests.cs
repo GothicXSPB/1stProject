a08e417b432fb1ae2bb481227b732021b95d7825
andreymuskat <andrey.strel.2001@gmail.com>
2022-12-17 21:23:39 +0300

    Add TestCaseSource for AddEmployee

--- 1stProject.Tests/AdminClassTests.cs
+++ 1stProject.Tests/AdminClassTests.cs
@@ -1,27 +1,18 @@
 using System.Xml;
 using _1stProject;
 using _1stProject.Options;
+using _1stProject.Tests.TestCaseSourse;
 using NUnit.Framework;
 
 namespace _1stProject.Tests
 {
     public class AdminClassTests
     {
-        [Test]
-        public void AddEmployeeTest()
+        [TestCaseSource(typeof(AddEmployeeTestsCaseSources))]
+        public void AddEmployeeTest(List<Employee> expectedEmployees, Employee employee, AdminClass admin)
         {
-            List<Employee> employees = new List<Employee>()
-            {
-                new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
-            };
+            admin.AddEmployee(employee);
 
-            Employee employee1 = new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2);
-            
-            AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
-
-            admin.AddEmployee(employee1);
-
-            List<Employee> expectedEmployees = employees;
             List<Employee> actualEmployees = Database.Employees;
             CollectionAssert.AreEqual(expectedEmployees, actualEmployees);
         }
@@ -29,7 +20,7 @@ namespace _1stProject.Tests
         [Test]
         public void DeleteEmployeeTest()
         {
-            List<Employee> employees1 = new List<Employee>()
+            List<Employee> employees2 = new List<Employee>()
             {
                 new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
             };
@@ -44,30 +35,34 @@ namespace _1stProject.Tests
 
             admin.DeleteEmployee(2);
 
-            List<Employee> expectedEmployees = employees1;
+            List<Employee> expectedEmployees = employees2;
             List<Employee> actualEmployees = Database.Employees;
             CollectionAssert.AreEqual(expectedEmployees, actualEmployees);
         }
 
-        [Test]
-        public void AddAdminTest()
-        {
-            List<Employee> employees1 = new List<Employee>()
-            {
-                new Employee(2, "Andrey", "88005553535", TimeTable.Shift5x2),
-            };
+        //[Test]
+        //public void AddAdminTest()
+        //{
+        //    List<Employee> employees3 = new List<Employee>()
+        //    {
+        //        new Employee(2, "Andrey", "88005553535", TimeTable.Shift5x2),
+        //    };
 
-            Database.Employees = new List<Employee>()
-            {
-              new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
-              new Employee(2, "Stepan", "88005553535", TimeTable.Shift2x2),
-              new Employee(3, "Irina", "88005553535", TimeTable.Shift1x3)
-            };
+        //    Database.Employees = new List<Employee>()
+        //    {
+        //      new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
+        //      new Employee(2, "Stepan", "88005553535", TimeTable.Shift2x2),
+        //      new Employee(3, "Irina", "88005553535", TimeTable.Shift1x3)
+        //    };
 
-            AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
+        //    AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
 
-            admin.AddAdmin(1);
-            admin.AddAdmin(3);
-        }
+        //    admin.AddAdmin(1);
+        //    admin.AddAdmin(3);
+
+        //    List<Employee> expectedEmployees = employees3;
+        //    List<Employee> actualEmployees = Database.Employees;
+        //    CollectionAssert.AreEqual(expectedEmployees, actualEmployees);
+        //}
     }
 }
--- 1stProject.Tests/TestCaseSources/AdminClassTestsCaseSources.cs
+++ 1stProject.Tests/TestCaseSources/AdminClassTestsCaseSources.cs
@@ -1,6 +1,23 @@
-﻿namespace _1stProject.Tests.TestCaseSourse
+﻿using System.Collections;
+using _1stProject.Options;
+
+namespace _1stProject.Tests.TestCaseSourse
 {
-    public class AdminClassTestsCaseSources
+    public class AddEmployeeTestsCaseSources : IEnumerable
     {
+        public IEnumerator GetEnumerator()
+        {
+            List<Employee> expectedEmployees = new List<Employee>()
+            {
+                new Employee(1, "Andrey Kokorin", "89032346789", TimeTable.Shift2x2)
+            };
+
+            Employee employee = new Employee(1, "Andrey Kokorin", "89032346789", TimeTable.Shift2x2);
+
+            AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
+
+            yield return new Object[] { expectedEmployees, employee, admin};
+        }
     }
+
 }
