efdb1773e47c08c2e406aafcb2bfb6fdcdc3ed77
andreymuskat <andrey.strel.2001@gmail.com>
2022-12-17 22:58:58 +0300

    Add new TestCaseSources

--- 1stProject.Tests/AdminClassTests.cs
+++ 1stProject.Tests/AdminClassTests.cs
@@ -17,52 +17,28 @@ namespace _1stProject.Tests
             CollectionAssert.AreEqual(expectedEmployees, actualEmployees);
         }
 
-        [Test]
-        public void DeleteEmployeeTest()
+        [TestCaseSource(typeof(DeleteEmployeeTestsCaseSources))]
+        public void DeleteEmployeeTest(List<Employee> expectedEmployees, List<Employee> employees, AdminClass admin)
         {
-            List<Employee> employees2 = new List<Employee>()
-            {
-                new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
-            };
-
-            Database.Employees = new List<Employee>()
-            {
-              new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
-              new Employee(2, "Stepan", "88005553535", TimeTable.Shift2x2),
-            };
-
-            AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
-
+            Database.Employees = employees;
             admin.DeleteEmployee(2);
-
-            List<Employee> expectedEmployees = employees2;
+            
             List<Employee> actualEmployees = Database.Employees;
             CollectionAssert.AreEqual(expectedEmployees, actualEmployees);
         }
 
-        //[Test]
-        //public void AddAdminTest()
-        //{
-        //    List<Employee> employees3 = new List<Employee>()
-        //    {
-        //        new Employee(2, "Andrey", "88005553535", TimeTable.Shift5x2),
-        //    };
+        [TestCaseSource(typeof(AddAdminTestsCaseSources))]
+        public void AddAdminTest(List<Employee> expectedEmployees, List<Employee> employees, List<Employee> expectedAdmins, AdminClass admin)
+        {
+            Database.Employees = employees;
+            admin.AddAdmin(1);
+            admin.AddAdmin(3);
 
-        //    Database.Employees = new List<Employee>()
-        //    {
-        //      new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
-        //      new Employee(2, "Stepan", "88005553535", TimeTable.Shift2x2),
-        //      new Employee(3, "Irina", "88005553535", TimeTable.Shift1x3)
-        //    };
+            List<Employee> actualEmployees = Database.Employees;
+            List<AdminClass> actualAdmins = Database.Admins;
+            CollectionAssert.AreEqual(expectedEmployees, actualEmployees);
 
-        //    AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
-
-        //    admin.AddAdmin(1);
-        //    admin.AddAdmin(3);
-
-        //    List<Employee> expectedEmployees = employees3;
-        //    List<Employee> actualEmployees = Database.Employees;
-        //    CollectionAssert.AreEqual(expectedEmployees, actualEmployees);
-        //}
+            CollectionAssert.AreEqual(expectedAdmins, actualAdmins);
+        }
     }
 }
--- 1stProject.Tests/TestCaseSources/AdminClassTestsCaseSources.cs
+++ 1stProject.Tests/TestCaseSources/AdminClassTestsCaseSources.cs
@@ -20,4 +20,53 @@ namespace _1stProject.Tests.TestCaseSourse
         }
     }
 
+    public class DeleteEmployeeTestsCaseSources : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            List<Employee> expectedEmployees = new List<Employee>()
+            {
+                new Employee(1, "Andrey Kokorin", "89032346789", TimeTable.Shift2x2),
+            };
+
+            List<Employee> Employees = new List<Employee>()
+            {
+                new Employee(1, "Andrey Kokorin", "89032346789", TimeTable.Shift2x2),
+                new Employee(2, "Maxim Ivanov", "88001039456", TimeTable.Shift5x2)
+            };
+
+            AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
+
+            yield return new Object[] { expectedEmployees, Employees, admin };
+        }
+    }
+
+    public class AddAdminTestsCaseSources : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            List<Employee> expectedEmployees = new List<Employee>()
+            {
+                new Employee(2, "Stepan", "88005553535", TimeTable.Shift2x2),
+            };
+
+            List<Employee>  employees = new List<Employee>()
+            {
+              new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
+              new Employee(2, "Stepan", "88005553535", TimeTable.Shift2x2),
+              new Employee(3, "Irina", "88005553535", TimeTable.Shift1x3)
+            };
+
+            List<Employee> expectedAdmins = new List<Employee>()
+            {
+              new Employee(1, "Andrey", "88005553535", TimeTable.Shift5x2),
+              new Employee(3, "Irina", "88005553535", TimeTable.Shift1x3)
+            };
+
+
+            AdminClass admin = new AdminClass(4, "Darya", "89301654545", TimeTable.Shift1x3);
+
+            yield return new Object[] { expectedEmployees, employees, expectedAdmins, admin };
+        }
+    }
 }
