3bcd0bd3eaa9c667ff48ca5a217cbf550bb393cf
andreymuskat <andrey.strel.2001@gmail.com>
2022-12-17 16:05:59 +0300

    Add class TelegramBotManager

---
+++ 1stProject/TelegramBotManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace _1stProject
+{
+    internal class TelegramBotManager
+    {
+        ITelegramBotClient _bot;
+
+        public TelegramBotManager()
+        {
+            string token = @"5910759542:AAHMbJh_wprscd-3TGi8T5kUaRwZG1LKB7s";
+            _bot = new TelegramBotClient(token);
+
+            Console.WriteLine("Запущен бот " + _bot.GetMeAsync().Result.FirstName);
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { },
+            };
+
+            _bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+           _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Привет, " + update.Message.Chat.FirstName);
+        }
+
+        public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+
+        }
+
+    }
+}
