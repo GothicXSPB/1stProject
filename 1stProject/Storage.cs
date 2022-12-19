087558db432faad871a4f60ef79eaf5559d094fe
Katerina Korotkaya <leanworld2020@gmail.com>
2022-12-19 16:48:40 +0300

    add method save and load

--- 1stProject/Storage.cs
+++ 1stProject/Storage.cs
@@ -7,5 +7,44 @@ namespace _1stProject
     {
         Dictionary<int, string> allCompany = new Dictionary<int, string>();
         Dictionary<int, List<int>> allWorker = new Dictionary<int, List<int>>();
+
+        public string _pathAllCompany = @"../InformationAllCompany/AllCompany.txt";
+        public string _pathAllWorker = @"../InformationAllWorker/AllWorker.txt";
+
+        public void SaveAllCompany()
+        {
+            using (StreamWriter sw = new StreamWriter(_pathAllCompany))
+            {
+                string jsn = JsonSerializer.Serialize(allCompany);
+                sw.WriteLine(jsn);
+            }
+        }
+
+        public void SaveAllWorker()
+        {
+            using (StreamWriter sw = new StreamWriter(_pathAllWorker))
+            {
+                string jsn = JsonSerializer.Serialize(allWorker);
+                sw.WriteLine(jsn);
+            }
+        }
+
+        public void LoadAllCompany()
+        {
+            using (StreamReader sr = new StreamReader(_pathAllCompany))
+            {
+                string jsn = sr.ReadLine()!;
+                allCompany = JsonSerializer.Deserialize<Dictionary<int, string>>(jsn)!;
+            }
+        }
+
+        public void LoadAllWorker()
+        {
+            using (StreamReader sr = new StreamReader(_pathAllWorker))
+            {
+                string jsn = sr.ReadLine()!;
+                allWorker = JsonSerializer.Deserialize<Dictionary<int, List<int>>>(jsn)!;
+            }
+        }
     }
 }
