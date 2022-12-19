170c24d4fffdac7fd36cbdbfe7778dd4f512e864
GothicXSPB <ZZZerggg@mail.ru>
2022-12-19 14:02:32 +0300

    fix programm

--- 1stProject/Program.cs
+++ 1stProject/Program.cs
@@ -8,12 +8,13 @@ Employee employee2 = new Employee(2, "Maxim Ivanov", "88001039456", TimeTable.Sh
 Employee employee3 = new Employee(3, "Irina Popova", "89152745869", TimeTable.Shift2x2);
 Employee employee4 = new Employee(4, "Katerina Apo", "84959238456", TimeTable.Shift1x3);
 Employee employee5 = new Employee(5, "Artem Titov", "89024569432", TimeTable.Shift1x3);
+Company company = new Company("Мяу", 3);
 
 AdminClass admin = new AdminClass(6, "Ivan Bobrov", "89301654545", TimeTable.Shift5x2);
 
 admin.AddEmployee(employee1);
 
-Company.SaveAllEmployees();
+company.SaveAllEmployees();
 
 admin.AddEmployee(employee2);
 admin.AddEmployee(employee3);
