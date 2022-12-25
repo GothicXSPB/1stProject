f49d0d56b6ed429a7a2185af39baa502ca0aa4ef
DariaKostrik <dolorez0927@yandex.ru>
2022-12-26 02:19:59 +0300

    Add all button for work(without  unique text desctiption)

--- 1stProject/TgButtonsLogic/MessagesFromTg.cs
+++ 1stProject/TgButtonsLogic/MessagesFromTg.cs
@@ -15,7 +15,7 @@ namespace _1stProject.TgButtonsLogic
             {
                 return new ModelOfMessage()
                 {
-                    Message = "Вас привествует система учета рабочего времени!",
+                    Message = "Доброго вам дня! Вас привествует система учета рабочего времени!",
                     Keyboard = new InlineKeyboardMarkup(
                         new[]
                         {
@@ -32,23 +32,75 @@ namespace _1stProject.TgButtonsLogic
 
         public static ModelOfMessage ShowMenuForAdmin
         {
+            get
+            {
+                return new ModelOfMessage()
+                {
+                    Message = "Ваши права администратора подтверждены. Прекласный сегодня денек! =) Что бы вы хотели сделать сегодня?",
+                    Keyboard = new InlineKeyboardMarkup(
+                        new[]
+                        {
+                            new[]
+                            {
+                                new InlineKeyboardButton("Я хочу ") { CallbackData = "хххх" },
+                                new InlineKeyboardButton("Я хочу") { CallbackData = "хх" }
+                            },
+                        })
+                };
+            }
+
 
         }
         public static ModelOfMessage ShowMenuForRegularUser
         {
-
+            get
+            {
+                return new ModelOfMessage()
+                {
+                    Message = "Прекрасный денек! Что бы вы хотели сделать сегодня? =)",
+                    Keyboard = new InlineKeyboardMarkup(
+                        new[]
+                        {
+                            new[]
+                            {
+                                new InlineKeyboardButton("Я хочу ") { CallbackData = "хххх" },
+                                new InlineKeyboardButton("Я хочу") { CallbackData = "хх" }
+                            },
+                        })
+                };
+            }
         }
-        public static ModelOfMessage ShowMenuForAdmin
-        {
-
-        }
+       
         public static ModelOfMessage ShowMenuForChoosingCompany
         {
+            get
+            {
+                return new ModelOfMessage()
+                {
+                    Message = "Выберите компанию в которой вы хотите продолжать работу",
+                    Keyboard = new InlineKeyboardMarkup(
+                        new[]
+                        {
+                            new[]
+                            {
+                                new InlineKeyboardButton("компания 1") { CallbackData = "хххх" },
+                                new InlineKeyboardButton("компания 2") { CallbackData = "хх" }
+                            },
+                        })
+                };
+            }
 
         }
         public static ModelOfMessage ShowMenuForCreatingNewCompany
         {
-
+            get
+            {
+                return new ModelOfMessage()
+                {
+                    Message = "Для создания компании укажите уникальное имя компании. Далее оно будет использоваться для авторизации вас и ваших сотрудников",
+                    Keyboard = null
+                };
+            }
         }
     }
 }
