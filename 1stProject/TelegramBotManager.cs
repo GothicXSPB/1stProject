a44bbe1584972defc46faecbb83584d41cb3fc8d
DariaKostrik <dolorez0927@yandex.ru>
2022-12-27 16:32:20 +0300

    Change HandleUpdateAsync

--- 1stProject/TelegramBotManager.cs
+++ 1stProject/TelegramBotManager.cs
@@ -3,6 +3,7 @@ using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
+using _1stProject.TgButtonsLogic;
 
 namespace _1stProject
 {
@@ -11,6 +12,8 @@ namespace _1stProject
         ITelegramBotClient _bot;
         public long UserTgId { get; set; }
         protected string _userText;
+        //private ActiveUserController _activeUserController;
+        private UserController _userController;
         public string UsersText 
         { 
             get
@@ -24,6 +27,7 @@ namespace _1stProject
         }
         public TelegramBotManager()
         {
+            //_activeUserController= new ActiveUserController();
             string token = @"5910759542:AAHMbJh_wprscd-3TGi8T5kUaRwZG1LKB7s";
             _bot = new TelegramBotClient(token);
 
@@ -47,53 +51,56 @@ namespace _1stProject
         public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             UserTgId = GetTgUserId(update);
+            ModelOfMessage message = _userController.GetReply(update);
+            await _bot.SendTextMessageAsync(UserTgId, message.Message, replyMarkup: message.Keyboard);
+
             //=======
 
-            Console.WriteLine(update.Message.Chat.FirstName + " " + update.Message.Chat.Id);
-            if (update.Message.Text == "/keyboard")
-            {
-                ReplyKeyboardMarkup keyboard = new(new[]
-                {
-                new KeyboardButton[] {"Меню"},
-                new KeyboardButton[] {"1", "2"}
-                })
-                {
-                    ResizeKeyboard = true
-                };
-                await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Выбирай:", replyMarkup: keyboard);
-                return;
-            }
+            //Console.WriteLine(update.Message.Chat.FirstName + " " + update.Message.Chat.Id);
+            //if (update.Message.Text == "/keyboard")
+            //{
+            //    ReplyKeyboardMarkup keyboard = new(new[]
+            //    {
+            //    new KeyboardButton[] {"Меню"},
+            //    new KeyboardButton[] {"1", "2"}
+            //    })
+            //    {
+            //        ResizeKeyboard = true
+            //    };
+            //    await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Выбирай:", replyMarkup: keyboard);
+            //    return;
+            //}
 
-            if (update.Message.Text == "/start")
-            {
-                _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Привет, я телеграмм бот менеджер,напиши" + "\r\n" + "/keyboard" + "\r\n" +
-                    $"1-Создать новую компанию" + "\r\n" + $"2-Зайти в существующую компанию");
-                return;
-            }
-            if (update.Message.Text == "Меню")
-            {
-                _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Привет, я телеграмм бот менеджер,напиши" + "\r\n" +
-                    "/keyboard" + "\r\n" +
-                    $"1-Создать новую компанию" + "\r\n" +
-                    $"2-Зайти в существующую компанию");
-                return;
-            }
-            if (update.Message.Text == "1")
-            {
-                _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Введи имя компании");
-                return;
-            }
-            if (update.Message.Text != null)
-            {
-                Company company1 = new Company(update.Message.Text, 11234); //(long)Convert.ToDouble(update.Message.Text)
-                AdminClass admin = new AdminClass(update.Message.Chat.Id, update.Message.Chat.FirstName, "123", 0);
-                admin.AddAdmin(admin);
-                Console.WriteLine(update.Message.Chat.FirstName + "Создал компанию " + company1.NameCompany + " " + company1.IdCompany);
+            //if (update.Message.Text == "/start")
+            //{
+            //    _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Привет, я телеграмм бот менеджер,напиши" + "\r\n" + "/keyboard" + "\r\n" +
+            //        $"1-Создать новую компанию" + "\r\n" + $"2-Зайти в существующую компанию");
+            //    return;
+            //}
+            //if (update.Message.Text == "Меню")
+            //{
+            //    _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Привет, я телеграмм бот менеджер,напиши" + "\r\n" +
+            //        "/keyboard" + "\r\n" +
+            //        $"1-Создать новую компанию" + "\r\n" +
+            //        $"2-Зайти в существующую компанию");
+            //    return;
+            //}
+            //if (update.Message.Text == "1")
+            //{
+            //    _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Введи имя компании");
+            //    return;
+            //}
+            //if (update.Message.Text != null)
+            //{
+            //    Company company1 = new Company(update.Message.Text, 11234); //(long)Convert.ToDouble(update.Message.Text)
+            //    AdminClass admin = new AdminClass(update.Message.Chat.Id, update.Message.Chat.FirstName, "123", 0);
+            //    admin.AddAdmin(admin);
+            //    Console.WriteLine(update.Message.Chat.FirstName + "Создал компанию " + company1.NameCompany + " " + company1.IdCompany);
 
-                _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Твоя комания:" + "\r\n" +
-                        "Name:" + company1.NameCompany + "\r\n" + "Id" + company1.IdCompany);
-                return;
-            }
+            //    _bot.SendTextMessageAsync(update.Message.Chat.Id, $"Твоя комания:" + "\r\n" +
+            //            "Name:" + company1.NameCompany + "\r\n" + "Id" + company1.IdCompany);
+            //    return;
+            //}
 
         }
         public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
--- 1stProject/TgButtonsLogic/ActiveUserController.cs
+++ 1stProject/TgButtonsLogic/ActiveUserController.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace _1stProject.TgButtonsLogic
 {
-    internal class ActiveUserController
+    public class ActiveUserController
     {
+       
     }
 }
