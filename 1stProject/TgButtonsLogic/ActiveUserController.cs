71fd16c889795e6b7574872c3ef5c634db94c186
DariaKostrik <dolorez0927@yandex.ru>
2022-12-27 17:33:09 +0300

    .3CreateBondsBetweenMethodsStagesAndButtons:

--- 1stProject/TgButtonsLogic/ActiveUserController.cs
+++ 1stProject/TgButtonsLogic/ActiveUserController.cs
@@ -6,8 +6,38 @@ using System.Threading.Tasks;
 
 namespace _1stProject.TgButtonsLogic
 {
-    public class ActiveUserController
+    internal class ActiveUserController
     {
-       
+        private Dictionary<long, UserController> _members;
+
+        public UserController this[long UserTgId]
+        {
+            get
+            {
+                return _members[UserTgId];
+            }
+        }
+
+        public ActiveUserController()
+        {
+            _members = new Dictionary<long, UserController>();
+        }
+
+        public void AddActiveMember(long UserTgId)
+        {
+            if (!_members.ContainsKey(UserTgId))
+            {
+                _members.Add(UserTgId, new UserController(UserTgId));
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public bool IsContais(long UserTgId)
+        {
+            return _members.ContainsKey(UserTgId);
+        }
     }
 }
