e6fa17f81a6524903f2a872d36960050929b96be
DariaKostrik <dolorez0927@yandex.ru>
2022-12-29 02:13:32 +0300

    change State

--- 1stProject/States/CreateCompany.cs
+++ 1stProject/States/CreateCompany.cs
@@ -11,23 +11,22 @@ namespace _1stProject.States
         //сделать первого юзера админом
         public ModelOfMessage HandleUpdate(Update update, UserController controller)
         {
-            ModelOfMessage message = MessagesFromTg.ShowMenuForCreatingNewCompany;
+            ModelOfMessage message = MessagesFromTg.ShowThatCompanyIsNotUnique;
             if (update.Type == UpdateType.Message)
             {
-                    Company company = new Company(update);
-                bool exists = company.IsThisCompanyAlreadyExist(update);
+                bool exists = IsThisCompanyAlreadyExist(update);
                 if (exists == false)
                 {
                     controller.State = new AddNewCompanyState();
                     message = MessagesFromTg.GreatCompany;
                 }
-                else if (exists == true)
-                {
-                    message = MessagesFromTg.ShowThatCompanyIsNotUnique;
-                }
             }
             return message;
         }
-
+        public bool IsThisCompanyAlreadyExist(Update update)
+        {
+            bool answer = Storage._storage.AllCompany.ContainsValue(update.Message.Text);
+            return answer;
+        }
     }
 }
