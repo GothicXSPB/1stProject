b5255fc10dadbd89cddc3c00c503764d960f2fa7
Katerina Korotkaya <leanworld2020@gmail.com>
2022-12-28 13:04:40 +0300

    test save and load idAdmin

--- 1stProject.Tests/CompanyTests.cs
+++ 1stProject.Tests/CompanyTests.cs
@@ -1,8 +1,58 @@
 ﻿using _1stProject;
+using _1stProject.Tests.TestCaseSources;
+using NUnit.Framework;
+using System.Text.Json;
 
 namespace _1stProject.Tests
 {
-    public class DatabaseTests
+    public class CompanyTests
     {
+        private string _pathCompanyTests;
+        private Company _company;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _pathCompanyTests = @"../AllTests.test";
+            _company = new Company("MultiTax", 6);
+            _company._pathAdmins = _pathCompanyTests;
+            _company._pathEmployees = _pathCompanyTests;
+            _company._pathCalendar = _pathCompanyTests;
+        }
+
+        [TestCaseSource(typeof(IdAdminsCompanyTestsCaseSources))]
+        public void SaveAllAdminsTests(List<long> IdAdmins)
+        {
+            _company.IdAdmins = IdAdmins;
+            _company.SaveAllAdmins();
+
+            List<long> expecredIdAdmins = _company.IdAdmins;
+            List<long> actualIdAdmins;
+
+            using (StreamReader sr = new StreamReader(_pathCompanyTests))
+            {
+                string jsn = sr.ReadLine()!;
+                actualIdAdmins = JsonSerializer.Deserialize<List<long>>(jsn)!;
+            }
+
+            CollectionAssert.AreEqual(expecredIdAdmins, actualIdAdmins);
+        }
+
+        [TestCaseSource(typeof(IdAdminsCompanyTestsCaseSources))]
+        public void LoadAllAdminsTests(List<long> IdAdmins)
+        {
+            using (StreamWriter sw = new StreamWriter(_pathCompanyTests))
+            {
+                string jsn = JsonSerializer.Serialize(IdAdmins);
+                sw.WriteLine(jsn);
+            }
+
+            _company.LoadAllAdmins();
+
+            List<long> expecredAllAdmins = IdAdmins;
+            List<long> actualAllAdmins = _company.IdAdmins;
+
+            CollectionAssert.AreEqual(expecredAllAdmins, actualAllAdmins);
+        }
     }
 }
--- 1stProject.Tests/TestCaseSources/CompanyTestsCaseSources.cs
+++ 1stProject.Tests/TestCaseSources/CompanyTestsCaseSources.cs
@@ -1,7 +1,20 @@
-﻿
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+
 namespace _1stProject.Tests.TestCaseSources
 {
-    public class DatabaseTestsCaseSources
+    public class IdAdminsCompanyTestsCaseSources : IEnumerable
     {
+        public IEnumerator GetEnumerator()
+        {
+            List<long> idAdmins = new List<long>() { 2205720832, 23652367582, 043040603456, 2638762863, 888882222, 0000000000 };
+            yield return new Object[] { idAdmins };
+
+            yield return new Object[]
+            {
+                    new List<long>()
+            };
+        }
     }
 }
